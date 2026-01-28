eddab32b14292e62e2e1565d52f37671d2b16ae3
Vasilica Olariu <olariu.vasilica@gmail.com>
2026-01-28 17:15:43 +0200

    regenerate prisma-client

--- packages/engagements-prisma-client/edge.js
+++ packages/engagements-prisma-client/edge.js
@@ -249,7 +249,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null,
+    "rootEnvPath": "../../.env",
     "schemaEnvPath": "../../.env"
   },
   "relativePath": "../../prisma",
